d20f581dc7df2e8fda50c27eb3dc932ac5c21929
student <student@intranet.proweb.uz>
2023-10-04 13:52:54 +0500

    added templates new model and media path

--- blog/web_site/views.py
+++ blog/web_site/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home_view(request):
-    return HttpResponse("hello world")
+    return render(request, 'web_site/index.html')
 
